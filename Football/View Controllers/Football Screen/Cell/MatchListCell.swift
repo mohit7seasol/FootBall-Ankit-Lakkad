813a5849937e4e8413537d181f6435f2bb91dfc4
mohit7seasol <mohit.7seasol@gmail.com>
2026-04-10 11:48:21 +0530

    added changes

--- Football/View Controllers/Football Screen/Cell/MatchListCell.swift
+++ Football/View Controllers/Football Screen/Cell/MatchListCell.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class MatchListCell: UICollectionViewCell {
 
+    @IBOutlet weak var dateView: UIView!
+    @IBOutlet weak var dateViewWidthConstant: NSLayoutConstraint!
+    @IBOutlet weak var dateLabel: UILabel! // Text Formate like when Live then '12 Jun • 06:30 PM', when upcoming then '06:30 PM', when Completed then '06:30 PM'
+    
+    @IBOutlet weak var teamAFlagImageView: UIImageView!
+    @IBOutlet weak var teamBFlagImageView: UIImageView!
+    @IBOutlet weak var vsImageView: UIImageView!
+    @IBOutlet weak var teamANameLabel: UILabel!
+    @IBOutlet weak var teamBNameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel! // Text Formate like when Live then '   ● LIVE   ', when upcoming then '   ● UPCOMING   ', when Completed then '   ● COMPLETED   '
+    
+    @IBOutlet weak var scorLabel: UILabel! // Text Formate like when Completed then '3  :  2'
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
